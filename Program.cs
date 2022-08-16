9fcd9bf5adfc802f9276b72a54550c57f42e42b4
Vadim Pudov <pvb@rambler.ru>
2022-08-16 13:40:25 +0700

    task23

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Clear();
+Console.WriteLine("Задача 23: программа, выдающая таблицу кубов чисел от 1 до введённого числа\n");
+
+while(true) {
+Console.WriteLine("Введите целое число:\n");
+if(int.TryParse(Console.ReadLine(), out int number)) {
+    for(int i = 0; i <= number; i++) {
+        Console.WriteLine(i + "^3 = " + Math.Pow(i, 3));
+    }
+    break;
+} else Console.WriteLine("Некорректно введено число!\n");
+}
